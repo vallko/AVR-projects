c8fdbc0a8d1a5d8475af7586533c1217dd0f4772
vallko <61822548+vallko@users.noreply.github.com>
2021-07-17 16:26:50 +0300

    Add files via upload

---
+++ ili9341/ili9341/Debug/main.d
@@ -0,0 +1,67 @@
+main.d main.o: .././main.c \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\io.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\sfr_defs.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\inttypes.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\stdint.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\stdint.h \
+ D:/Program\ Files\ (x86)/Atmel/Studio/7.0/Packs/atmel/ATmega_DFP/1.6.364/xc8/avr/include/avr/iom328p.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\portpins.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\common.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\version.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\fuse.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\lock.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\util\delay.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\util\delay_basic.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\math.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\stdio.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\stdarg.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\stddef.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\stdbool.h \
+ .././ili9341.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\pgmspace.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include-fixed\limits.h \
+ .././ili9341gfx.h
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\io.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\sfr_defs.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\inttypes.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\stdint.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\stdint.h:
+
+D:/Program\ Files\ (x86)/Atmel/Studio/7.0/Packs/atmel/ATmega_DFP/1.6.364/xc8/avr/include/avr/iom328p.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\portpins.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\common.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\version.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\fuse.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\lock.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\util\delay.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\util\delay_basic.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\math.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\stdio.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\stdarg.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\stddef.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\stdbool.h:
+
+.././ili9341.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\pgmspace.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include-fixed\limits.h:
+
+.././ili9341gfx.h:
